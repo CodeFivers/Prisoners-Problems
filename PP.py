15ba5514d23c88390044e7a18256d9b4f1dc098d
wldwjd <wlswjd010629@gmail.com>
2025-04-23 18:46:40 +0900

    feat: Genaric Part of Prisoners-Problems

---
+++ PP.py
@@ -0,0 +1,56 @@
+# Just :: a -> Maybe a
+def Just(x):
+    '''Constructor for an inhabited Maybe (option type) value.
+       Wrapper containing the result of a computation.
+    '''
+    return {'type': 'Maybe', 'Nothing': False, 'Just': x}
+
+
+# Nothing :: Maybe a
+def Nothing():
+    '''Constructor for an empty Maybe (option type) value.
+       Empty wrapper returned where a computation is not possible.
+    '''
+    return {'type': 'Maybe', 'Nothing': True}
+
+
+# difference :: Eq a => [a] -> [a] -> [a]
+def difference(xs):
+    '''All elements of xs, except any also found in ys.'''
+    return lambda ys: list(set(xs) - set(ys))
+
+
+# randomRInt :: Int -> Int -> IO () -> Int
+def randomRInt(m):
+    '''The return value of randomRInt is itself
+       a function. The returned function, whenever
+       called, yields a a new pseudo-random integer
+       in the range [m..n].
+    '''
+    return lambda n: lambda _: randint(m, n)
+
+
+# unfoldr(lambda x: Just((x, x - 1)) if 0 != x else Nothing())(10)
+# -> [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+def unfoldr(f):
+    '''Dual to reduce or foldr.
+       Where catamorphism reduces a list to a summary value,
+       the anamorphic unfoldr builds a list from a seed value.
+       As long as f returns Just(a, b), a is prepended to the list,
+       and the residual b is used as the argument for the next
+       application of f.
+       When f returns Nothing, the completed list is returned.
+    '''
+    def go(v):
+        xr = v, v
+        xs = []
+        while True:
+            mb = f(xr[0])
+            if mb.get('Nothing'):
+                return xs
+            else:
+                xr = mb.get('Just')
+                xs.append(xr[1])
+        return xs
+    return lambda x: go(x)
